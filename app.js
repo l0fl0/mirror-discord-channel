789ee11a1979a3deba77ec56bd3f85037bd1d5b2
Louis Flores <louisdefleurs@gmail.com>
2022-05-04 23:36:14 -0400

    this is the code

--- app.js
+++ app.js
@@ -1,3 +1,27 @@
-require("dotenv").config()
-const discord = require("discord.js-self");
+require("dotenv").config();
+const Discord = require("discord.js");
+const bot = new Discord.Client()
+const realmsBuffHook = new Discord.WebhookClient(process.env.BUFF_WEBHOOK_ID, process.env.BUFF_WEBHOOK_TOKEN);
+// make get request @ the webhook url to get id and token
+
+
+
+bot.on("ready", () => {
+  console.log(`Logged in as ${bot.user.tag}!`);
+});
+
+bot.on("message", msg => {
+  // 520163873022672906 Dr Broadcast
+  if (msg.channel.id === process.env.DR_CHANNEL_ID) {
+    // channel to scrape messages
+    let content = msg.content ? msg.content : { embed: [msg.embeds[0]] };
+
+    if (/buff/i.test(content)) realmsBuffHook.send(content);
+
+  }
+});
+
+
+bot.login(process.env.DISCORD_TOKEN);
+
 
